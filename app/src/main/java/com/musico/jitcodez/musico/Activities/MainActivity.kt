6a06e0e8f3fd98613b447efb87420cb76ebafc8f
Jitendra <Jitendra93266@gmail.com>
2018-02-07 23:26:57 +0530

    Notification Manager

--- app/src/main/java/com/musico/jitcodez/musico/Activities/MainActivity.kt
+++ app/src/main/java/com/musico/jitcodez/musico/Activities/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.musico.jitcodez.musico.Activities
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -11,6 +17,7 @@ import android.support.v7.widget.Toolbar
 import android.widget.LinearLayout
 import com.musico.jitcodez.musico.Adapters.NavigationDrawer_Adapter
 import com.musico.jitcodez.musico.Fragments.MainScreenFragment
+import com.musico.jitcodez.musico.Fragments.SongPlayingFragment
 import com.musico.jitcodez.musico.R
 
 
@@ -19,8 +26,13 @@ class MainActivity : AppCompatActivity() {
     var navigationTextList:ArrayList<String> = arrayListOf();
     object statified{
         var drawerLayout:DrawerLayout?=null;
+        var notificationManager:NotificationManager?=null
+
 
     }
+
+    var trackNotificationBuilder: Notification?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
@@ -50,10 +62,51 @@ class MainActivity : AppCompatActivity() {
         nav_rv.itemAnimator=DefaultItemAnimator()
         nav_rv.adapter=navAdapter;
         nav_rv.setHasFixedSize(true)
+
+        val intent= Intent(this@MainActivity,MainActivity::class.java)
+        val pIntent=PendingIntent.getActivity(this@MainActivity,System.currentTimeMillis() as Int,
+                intent,0)
+        trackNotificationBuilder=Notification.Builder(this)
+                .setContentTitle("A track is played in background")
+                .setSmallIcon(R.drawable.play_icon)
+                .setContentIntent(pIntent)
+                .setOngoing(true)
+                .setAutoCancel(true)
+                .build()
+
+        statified.notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 
     override fun onStart() {
         super.onStart()
+        try{
+            statified.notificationManager?.cancel(1978)
+        }catch(e:Exception)
+        {
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        try{
+            if( SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
+                statified.notificationManager?.notify(1978,trackNotificationBuilder)
+            }
+        }catch(e:Exception)
+        {
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        try{
+            statified.notificationManager?.cancel(1978)
+        }catch(e:Exception)
+        {
+
+        }
     }
 }
 
--- app/src/main/java/com/musico/jitcodez/musico/utils/CaptureBroadcast.kt
+++ app/src/main/java/com/musico/jitcodez/musico/utils/CaptureBroadcast.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.telephony.TelephonyManager
+import com.musico.jitcodez.musico.Activities.MainActivity
 import com.musico.jitcodez.musico.Fragments.SongPlayingFragment
 import com.musico.jitcodez.musico.R
 import kotlinx.android.synthetic.main.fragment_favorite.view.*
@@ -12,38 +13,48 @@ import kotlinx.android.synthetic.main.fragment_favorite.view.*
 /**
  * Created by jitu on 7/2/18.
  */
-class CaptureBroadcast:BroadcastReceiver(){
+class CaptureBroadcast : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if(intent?.action==Intent.ACTION_NEW_OUTGOING_CALL)
-        {
+        if (intent?.action == Intent.ACTION_NEW_OUTGOING_CALL) {
+            try{
+            MainActivity.statified.notificationManager?.cancel(1978)
+            }catch(e:Exception)
+            {
+
+            }
+
+
             try {
                 if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
                     SongPlayingFragment.Statified.mediaPlayer?.pause()
                     SongPlayingFragment.Statified.playpauseImageButton?.setBackgroundResource(R.drawable.play_icon)
                 }
-            }catch(e:Exception)
-            {
+            } catch (e: Exception) {
 
             }
-        }else{//Telephony
-val tm:TelephonyManager=context?.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
-        when(tm?.callState)
-        {
-            TelephonyManager.CALL_STATE_RINGING->{
-                try{
-                    if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
-                        SongPlayingFragment.Statified.mediaPlayer?.pause()
-                        SongPlayingFragment.Statified.playpauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+        } else {//Telephony
+            val tm: TelephonyManager = context?.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
+            when (tm?.callState) {
+                TelephonyManager.CALL_STATE_RINGING -> {
+                    try{
+                        MainActivity.statified.notificationManager?.cancel(1978)
+                    }catch(e:Exception)
+                    {
+
                     }
-                }catch(e:Exception)
-                {
+                    try {
+                        if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
+                            SongPlayingFragment.Statified.mediaPlayer?.pause()
+                            SongPlayingFragment.Statified.playpauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+                        }
+                    } catch (e: Exception) {
+
+                    }
+                }
+                else -> {
 
                 }
             }
-            else->{
-
-            }
-        }
 
         }
 
